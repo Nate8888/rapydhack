6588efa2e8050832fc33bc23a553d3962b759f78
Nate8888 <wilk.nathan@gmail.com>
2021-07-06 13:00:43 -0400

    auth calculation

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,36 @@ import datetime
 import requests
 from flask import Flask, render_template, request, jsonify, redirect
 
+base_url = 'https://sandboxapi.rapyd.net'
+access_key = '<your-access-key>'
+secret_key = '<your-secret-key>'
+
+
+def generate_salt(length=12):
+    return ''.join(random.sample(string.ascii_letters + string.digits, length))
+
+def get_unix_time(days=0, hours=0, minutes=0, seconds=0):
+    return int(time.time())
+
+def update_timestamp_salt_sig(http_method, path, body):
+    if path.startswith('http'):
+        path = path[path.find(f'/v1'):]
+    salt = generate_salt()
+    timestamp = get_unix_time()
+    to_sign = (http_method, path, salt, str(timestamp), access_key, secret_key, body)
+
+    h = hmac.new(secret_key.encode('utf-8'), ''.join(to_sign).encode('utf-8'), hashlib.sha256)
+    signature = base64.urlsafe_b64encode(str.encode(h.hexdigest()))
+    return salt, timestamp, signature
+
+def current_sig_headers(salt, timestamp, signature):
+    sig_headers = {'access_key': access_key,
+                   'salt': salt,
+                   'timestamp': str(timestamp),
+                   'signature': signature,
+                   'idempotency': str(get_unix_time()) + salt}
+    return sig_headers
+
 def get_auth_headers():
     #Todo: add auth salting
     headers = {
